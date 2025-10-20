a2dda00b45aaf58a99ade6fa6c6d2dfc9260dffb
Ninii <eaninika@gmail.com>
2025-10-20 15:48:44 -0300

    DEI COMMIT PORRA NAO FODE COM MEU TRABALHO CARALHO

--- variaveis.php
+++ variaveis.php
@@ -1,1 +1,27 @@
 <?php
+    echo 'oi mundo <br>';
+    echo '<hr>';
+    print 'oi mundo';
+    echo '<hr>';
+    echo '<p style="font-family: Microsoft JhengHei, sans-serif">ksdarqw pa dfiiushsifu mkl</p>';
+
+    $nome = 'jon';
+    $sobreNome = 'doe';
+    $nomeCompleto = $nome . ' ' . $sobreNome;
+    echo '<br>';
+    echo ($nomeCompleto);
+    var_dump($nomeCompleto);
+
+    $cpf = '123456789';
+    var_dump($cpf);
+
+    if ($cpf == '123456789'); {
+        echo 'cpf estranho';
+    }else{
+        echo 'Com esse CPF mais R$ 14 reais voce entra no cinema na terça!';
+    }
+
+    function soma($peso, $altura)
+    {
+
+    }
