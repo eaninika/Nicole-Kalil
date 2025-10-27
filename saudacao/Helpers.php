a52b7a3eb528ecd5792a86d6ddb991b455bf72e0
Ninii <eaninika@gmail.com>
2025-10-27 15:49:51 -0300

    commit commit commit commit commit commmit commmit commit commit commit commit commit commmit commit commit commit commit commit commit commit commit commit commmit commmit commit commit commit commit commit commmit commit commit commit commit commit commit commit commit commit commmit commmit commit commit commit commit commit commmit commit commit commit commit

--- saudacao/Helpers.php
+++ saudacao/Helpers.php
@@ -18,3 +18,24 @@ function saudacao(): string
     return $saudacao;
 
 }
+function contarTempo(string $data) : string {
+    $agora = strtotime(date('Y-m-d H:i:s'));
+    $tempo = strtotime($data);
+
+
+    echo('Data do PC ' . $agora);
+    echo "<hr>";
+    echo('Data digitada ' . $tempo)
+    $diferenca = $agora - $tempo;
+    echo "<hr>";
+    echo('Diferença ' . $diferenca);
+
+    $segundos = $diference;
+    $minutos = round(num: $diference / 60);// um minuto tem 60
+    $horas = round(num: $diference / 3600);// uma hora tem 60 minutos
+    $dias = round(num: $diference / 86400);// um dia tem 24 horas
+    $semanas = round(num: $diference / 604800);// uma semana tem sete dias
+    $meses = round(num: $diference / 2419200);// um mês tem quatr semanas
+    $anos = round(num: $diference / 29030400);// um ano tem doze meses
+}
+
