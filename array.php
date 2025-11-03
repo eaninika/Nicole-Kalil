2bc8359075e21759785399d73243caee46de4f0e
Ninii <eaninika@gmail.com>
2025-11-03 11:29:53 -0300

    commit commit commit commit commit commmit commmit commit commit commit commit commit commmit commit commit commit commit commit commit commit commit commit commmit commmit commit commit commit commit commit commmit commit commit commit commit commit commit commit commit commit commmit commmit commit commit commit commit commit commmit commit commit commit commit commit commit commit commit commit commmit commmit commit commit commit commit commit commmit commit commit commit commit commit commit commit commit commit commmit commmit commit commit commit commit commit commmit commit commit commit commit commit commit commit commit commit commmit commmit commit commit commit commit commit commmit commit commit commit commit commit commit commit commit commit commmit commmit commit commit commit commit commit commmit commit commit commit commit commit commit commit commit commit commmit commmit commit commit commit commit commit commmit commit commit commit commit commit commit commit commit commit commmit commmit commit commit commit commit commit commmit commit commit commit commit commit commit commit commit commit commmit commmit commit commit commit commit commit commmit commit commit commit commit commit commit commit commit commit commmit commmit commit commit commit commit commit commmit commit commit commit commit commit commit commit commit commit commmit commmit commit commit commit commit commit commmit commit commit commit commit

--- array.php
+++ array.php
@@ -5,5 +5,33 @@
     $mes = [
         'Janeiro', 'fevereiro', 'marco', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'
     ];
+echo 'CHAVE E VALOR<br>';
+foreach ($mes as $key => $value){
+    echo $key . ' : ' . $value . "<br>";
+}
 
-    echo $mes[date('n')-1];
+    echo '<hr>';
+    echo 'VAR_DUMP';
+    var_dump($mes);
+    echo '<hr>';
+    echo 'Posição 1 no array MÊS';
+    echo $mes[1];
+    echo '<hr>';
+
+ //data
+function dataAtual(): string
+{
+ $diaMes = date('d');
+ $diaSemana = date('w');
+ $mes = date('m');
+ $ano = date('Y');
+
+ $nomeDosMeses = ['janeiro', 'fevereiro', 'marco', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
+
+ $nomeDiasDaSemana = ['domingo' , 'segunda-feira' , 'terça-feira' , 'quarta-feira' , 'quinta-feira' , 'sexta-feira' , 'sabado'];
+
+ $dataFormatada = $nomeDiasDaSemana[$diaSemana] . ', ' . $diaMes . ' de ' . $nomeDosMeses[$mes - 1] . ' de ' . $ano;
+
+ return $dataFormatada;
+}
+    echo dataAtual();
--- saudacao/Helpers.php
+++ saudacao/Helpers.php
@@ -25,17 +25,16 @@ function contarTempo(string $data) : string {
 
     echo('Data do PC ' . $agora);
     echo "<hr>";
-    echo('Data digitada ' . $tempo)
+    echo('Data digitada ' . $tempo);
     $diferenca = $agora - $tempo;
     echo "<hr>";
     echo('Diferença ' . $diferenca);
 
-    $segundos = $diference;
-    $minutos = round(num: $diference / 60);// um minuto tem 60
-    $horas = round(num: $diference / 3600);// uma hora tem 60 minutos
-    $dias = round(num: $diference / 86400);// um dia tem 24 horas
-    $semanas = round(num: $diference / 604800);// uma semana tem sete dias
-    $meses = round(num: $diference / 2419200);// um mês tem quatr semanas
-    $anos = round(num: $diference / 29030400);// um ano tem doze meses
+    $segundos = $diferenca;{
+    $minutos = round(num: $diferenca / 60);// um minuto tem 60
+    $horas = round(num: $diferenca / 3600);// uma hora tem 60 minutos
+    $dias = round(num: $diferenca / 86400);// um dia tem 24 horas
+    $semanas = round(num: $diferenca / 604800);// uma semana tem sete dias
+    $meses = round(num: $diferenca / 2419200);// um mês tem quatr semanas
+    $anos = round(num: $diferenca / 29030400);// um ano tem doze meses
 }
-
